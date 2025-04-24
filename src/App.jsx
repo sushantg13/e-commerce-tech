06c3a98c44fa5d563d3b0e76cad68c024c0dc1f4
Sushant <sushantg@student.unimelb.edu.au>
2025-04-24 23:49:34 +1000

    Added basic Styles in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    // this is an asynchronous call so the return value is actually a promise
+    fetch("http://localhost:3001/categories")
+      .then(response => response.json()) 
+      .then(data => {
+        console.log(data); 
+        setResults(data); 
+      });
+  }, []);
+
+ 
   return (
     <>
-      E-Commerce project setup.
+      <header>My Store</header>
+      
+      <section>
+        <nav>
+        {
+          results.map( (d) => (
+            <div key={d.id}>{d.title}</div>
+          ))
+        }
+        </nav>
+
+        <article>
+          Main Area
+        </article>
+        <footer>Footer</footer>
+      </section>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar() {
+    return (<div>
+        Hey this is sushant. 
+        <h2> This is a course abotu something.</h2>
+    </div>);
+}
+
+export default Navbar;
